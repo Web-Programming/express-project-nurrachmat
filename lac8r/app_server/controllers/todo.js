9d85e3d588c91eba47fc287acfe931a6a97f9b60
nurrachmat-nr <rachmat.nur91@gmail.com>
2023-10-25 14:50:14 +0700

    CRUD

--- lac8r/app_server/controllers/todo.js
+++ lac8r/app_server/controllers/todo.js
@@ -15,7 +15,7 @@ const index = (req, res, next) => {
       .catch((e) => {
         const responseMessage = {
             code: 400,
-            success: true,
+            success: false,
             message: "Bad request."
         };
         res.status(400).json(responseMessage);
@@ -48,6 +48,123 @@ const insert = (req, res, next) => {
         });
 };
 
+//untuk menghandle request update todo
+const update = (req, res, next) => {
+
+     Todo
+        .findByIdAndUpdate(req.params.id, {text: req.body.text})
+        .then((todo) => {
+            Todo
+            .findById(req.params.id)
+            .then((todo) =>{
+                const responseMessage = {
+                    code: 200,
+                    success: true,
+                    message: "Successfull",
+                    data: todo
+                };
+                res.status(200).json(responseMessage);
+            })
+            /*const responseMessage = {
+                code: 200,
+                success: true,
+                message: "Successfully updated",
+                data: todo
+            };
+            res.status(200).json(responseMessage);     */           
+        })
+        .catch((e) => {
+            const responseMessage = {
+                code: 404,
+                success: false,
+                message: "ID " + req.params.id + " Not Found",
+                error: e
+            };
+            res.status(404).json(responseMessage);
+        });
+    /*Todo
+        .findById(req.params.id)
+        .then((todo) => {
+            todo.text =  req.body.text; //set field / property yg ingin di update 
+            todo
+                .save()      //panggil .save() untuk menyimpan    
+                .then((result) => {
+                    const responseMessage = {
+                        code: 200,
+                        success: true,
+                        message: "Successfully updated",
+                        data: result
+                    };
+                    res.status(200).json(responseMessage);
+                })
+                .catch((e) => {
+                    const responseMessage = {
+                        code: 400,
+                        success: false,
+                        message: "Update failed"
+                    };
+                    res.status(400).json(responseMessage);
+                });
+        })
+        .catch((e) => {
+            const responseMessage = {
+                code: 404,
+                success: false,
+                message: "ID " + req.params.id + " Not Found",
+                error: e
+            };
+            res.status(404).json(responseMessage);
+        });*/
+};
+
+
+//untuk menghandle request show detail
+const show = (req, res, next) => {
+    Todo
+        .findById(req.params.id)
+        .then((todo) =>{
+            const responseMessage = {
+                code: 200,
+                success: true,
+                message: "Successfull",
+                data: todo
+            };
+            res.status(200).json(responseMessage);
+        })
+        .catch((e) => {
+            const responseMessage = {
+                code: 404,
+                success: false,
+                message: "ID " + req.params.id + " Not Found",
+            };
+            res.status(404).json(responseMessage);
+        });
+};
+
+
+//untuk menghandle request delete
+const destroy = (req, res, next) => {
+    Todo
+        .findByIdAndDelete(req.params.id)
+        .then((todo) => {
+            const responseMessage = {
+                code: 200,
+                success: true,
+                message: "Delete Successfull",
+            };
+            res.status(200).json(responseMessage);
+        });
+        /*.catch((e) => {
+            const responseMessage = {
+                code: 404,
+                success: false,
+                message: "ID " + req.params.id + " Not Found",
+                error: e
+            };
+            res.status(404).json(responseMessage);
+        });*/
+};
+
 module.exports = {
-    index, insert
+    index, insert, update, show, destroy
 }
--- lac8r/app_server/routes/todo.js
+++ lac8r/app_server/routes/todo.js
@@ -4,5 +4,8 @@ const todoController = require('../controllers/todo');
 
 router.get('/', todoController.index);  //list todos
 router.post('/insert', todoController.insert);  //insert todos
+router.put('/update/:id', todoController.update);  //mengupdate todos
+router.get('/show/:id', todoController.show);  //show detail todos by id
+router.delete('/delete/:id', todoController.destroy) //delete todos by id
 
 module.exports = router;
